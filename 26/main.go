bac06a15bdec94036b604f837c1c21aa2128db0c
mengboy <bai.white86@gmail.com>
2018-10-19 22:19:18 +0800

    update

--- 26/main.go
+++ 26/main.go
@@ -1,1 +1,43 @@
-package _6
+package main
+
+import (
+	"fmt"
+)
+
+/**
+* 删除数组重复项
+ */
+
+func main()  {
+	arr := []int{
+		1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5,
+	}
+	fmt.Println(removeDuplicates(arr))
+}
+
+
+func removeDuplicates(nums []int) int {
+	l := len(nums)
+
+	if l == 0 || l == 1{
+		if l == 1{
+			fmt.Println(nums[0])
+		}
+		return l
+	}
+
+	i := 1
+	for j := 0; j < l - 1; j++{
+		if nums[j] == nums[j + 1]{
+			continue
+		}else {
+			nums[i] = nums[j + 1]
+			i++
+	 	}
+	}
+
+	for j := 0; j < i - 1; j++{
+		fmt.Println(nums[j])
+	}
+	return i
+}
